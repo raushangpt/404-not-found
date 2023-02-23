438aad6c20317c26447239bb1aac473688493bc1
raushangpt <rk880488@gmail.com>
2023-02-23 22:52:02 +0530

    Initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    fontFamily: {
+      'space-mono': ['Space Mono', 'monospace'],
+      'inconsolata': ['Inconsolata', 'monospace'],
+    },
+    screens: {
+      'sm': '425px',
+      'lg': '1024px',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
